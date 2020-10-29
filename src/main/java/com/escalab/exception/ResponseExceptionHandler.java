3f3e71b003f93b525015653096af0a285a6ddce2
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-10-29 08:43:51 -0300

    aspectos finales de backend y reportes

--- src/main/java/com/escalab/exception/ResponseExceptionHandler.java
+++ src/main/java/com/escalab/exception/ResponseExceptionHandler.java
@@ -16,20 +16,18 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 	
-	// 3 formas de manejo
-	
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<ExceptionResponse> manejarTodasExcepciones(ModeloNotFoundException ex, WebRequest request) {
+	public final ResponseEntity<ExceptionResponse> manejarTodasExcepciones(ModeloNotFoundException ex, WebRequest request ) {
 		ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<ExceptionResponse>(er, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	@ExceptionHandler(ModeloNotFoundException.class)
 	public final ResponseEntity<ExceptionResponse> manejarModeloException(ModeloNotFoundException ex, WebRequest request) {
-		ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false)); 
+		ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<ExceptionResponse>(er, HttpStatus.NOT_FOUND);
 	}
-	
+
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
--- src/main/java/com/escalab/repo/IMenuRepo.java
+++ src/main/java/com/escalab/repo/IMenuRepo.java
@@ -10,7 +10,7 @@ import com.escalab.model.Menu;
 
 public interface IMenuRepo extends JpaRepository<Menu, Integer> {
 	
-	@Query(value = "select m.* from menu_rol mr inner join usuario_rol ur on ur.id_rol = mr.id_rol inner join menu m on m.id_menu = mr.id_menu inner join usuario u on u.id_usuario = ur.id_usuario where u.username = :nombre", nativeQuery = true)
+	@Query(value="select m.* from menu_rol mr inner join usuario_rol ur on ur.id_rol = mr.id_rol inner join menu m on m.id_menu = mr.id_menu inner join usuario u on u.id_usuario = ur.id_usuario where u.nombre = :nombre", nativeQuery = true)
 	List<Object[]> listarMenuPorUsuario(@Param("nombre") String nombre);
 
 }
